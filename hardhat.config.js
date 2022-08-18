903241d7a55f858bc6772b472b4376e0f47395bf
miketsui3a <42023809+miketsui3a@users.noreply.github.com>
2022-08-19 00:12:28 +0800

    asdf

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
-require('dotenv').config()
+require("dotenv").config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -18,12 +18,107 @@ task("accounts", "Prints the list of accounts", async () => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.5.16",
+  solidity: {
+    compilers: [
+      {
+        version: "0.5.16",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000,
+          },
+        },
+      },
+      {
+        version: "0.6.7",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000,
+          },
+        },
+      },
+    ],
+    overrides: {
+      "contracts/UniswapV2Router02.sol": {
+        version: "0.6.6",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000,
+          },
+        },
+      },
+      "contracts/UniswapV2Router01.sol": {
+        version: "0.6.6",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000,
+          },
+        },
+      },
+      "contracts/DummyToken.sol": {
+        version: "0.8.0",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+      "@openzeppelin/contracts/token/ERC20/ERC20.sol": {
+        version: "0.8.0",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+      "@openzeppelin/contracts/utils/Context.sol": {
+        version: "0.8.0",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+      "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol": {
+        version: "0.8.0",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+      "@openzeppelin/contracts/token/ERC20/IERC20.sol": {
+        version: "0.8.0",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    },
+  },
   networks: {
+    hardhat: {
+      forking: {
+        url: `https://rpc01-sg.dogechain.dog`,
+        accounts: [process.env.PRIVATE_KEY],
+      },
+    },
     ftmTest: {
       url: `https://rpc01-sg.dogechain.dog`,
       accounts: [process.env.PRIVATE_KEY],
     },
+    local: {
+      url: `http://127.0.0.1:8545`,
+      accounts: [process.env.PRIVATE_KEY],
+    },
   },
 };
-
--- test/sample-test.js
+++ test/sample-test.js
@@ -1,18 +1,89 @@
 const { expect } = require("chai");
+const { parseEther } = require("ethers/lib/utils");
+require("dotenv").config();
 
-describe("Greeter", function() {
-  it("Should return the new greeting once it's changed", async function() {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe("Greeter", function () {
+  it("Should return the new greeting once it's changed", async function () {
+    const signer = await hre.ethers.getSigner();
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+    const UniswapV2Factory = await ethers.getContractFactory(
+      "UniswapV2Factory"
+    );
+    const UniswapV2Router02 = await ethers.getContractFactory(
+      "UniswapV2Router02"
+    );
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
-    
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+    const DummyToken = await ethers.getContractFactory("DummyToken");
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    const uniswapV2Factory = await UniswapV2Factory.deploy(signer.address);
+    await uniswapV2Factory.deployed();
+
+    const uniswapV2Router02 = await UniswapV2Router02.deploy(
+      uniswapV2Factory.address,
+      process.env.WETH
+    );
+    await uniswapV2Router02.deployed();
+
+    const dummyToken = await DummyToken.deploy();
+    await dummyToken.deployed();
+
+    const dummyToken2 = await DummyToken.deploy();
+    await dummyToken2.deployed();
+
+    await (
+      await uniswapV2Factory.createPair(dummyToken.address, dummyToken2.address)
+    ).wait();
+
+    await (
+      await dummyToken.approve(uniswapV2Router02.address, parseEther("100.0"))
+    ).wait();
+
+    await (
+      await dummyToken2.approve(uniswapV2Router02.address, parseEther("100.0"))
+    ).wait();
+    await (
+      await uniswapV2Router02.addLiquidity(
+        dummyToken.address,
+        dummyToken2.address,
+        parseEther("1.0"),
+        parseEther("1.0"),
+        parseEther("1.0"),
+        parseEther("1.0"),
+        signer.address,
+        "99999999999"
+      )
+    ).wait();
+
+    const pair = await uniswapV2Factory.getPair(
+      dummyToken.address,
+      dummyToken2.address
+    );
+
+    const abi = ["function approve(address, uint256) returns (bool)"];
+
+    const lp = new ethers.Contract(pair, abi, signer);
+    await (
+      await lp.approve(uniswapV2Router02.address, parseEther("1000.0"))
+    ).wait();
+    await (
+      await uniswapV2Router02.removeLiquidity(
+        dummyToken.address,
+        dummyToken2.address,
+        parseEther("0.000000001"),
+        parseEther("0.0"),
+        parseEther("0.0"),
+        signer.address,
+        "99999999999"
+      )
+    ).wait();
+
+    // expect(await greeter.greet()).to.equal("Hello, world!");
+
+    // const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+
+    // // wait until the transaction is mined
+    // await setGreetingTx.wait();
+
+    // expect(await greeter.greet()).to.equal("Hola, mundo!");
   });
 });
